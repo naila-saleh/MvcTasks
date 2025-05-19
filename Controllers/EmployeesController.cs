9a32fb05c9587fb71e734f7aae26dba1d4a6cabe
naila-saleh <nailasaleh2004@gmail.com>
2025-05-19 12:25:17 +0300

    mvc controller&view create

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+        public ViewResult Edit()
+        {
+            return View();
+        }
+    }
+}
